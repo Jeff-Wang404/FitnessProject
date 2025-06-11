156f0e9f2f3186fb16c599d618b46b3ca2f335ea
Andrew Park <joohyung122002@gmail.com>
2025-06-11 16:01:04 -0700

    bluetooth progress

--- fitness_project/lib/screens/connectscreen.dart
+++ fitness_project/lib/screens/connectscreen.dart
@@ -22,25 +22,103 @@ class _ConnectScreenState extends State<ConnectScreen>
   static Guid serviceUuid = Guid('6E400001-B5A3-F393-E0A9-E50E24DCCA9E');
   bool isScanning = false;
   List<ScanResult> scanResults = [];
-  StreamSubscription<ScanResult>? scanSubscription;
+  late StreamSubscription<List<ScanResult>> scanSubscription;
   String statusMessage = "Not Connected";
+  BluetoothDevice? _connectedDevice;
 
   // late AnimationController _animationController;
   late Animation<double> _pulseAnimation;
 
   // function to check if already connected
+  Future<void> _checkConnectedDevices() async {
+    try {
+      final devices = await FlutterBluePlus.connectedDevices;
+      if (devices.isNotEmpty) {
+        for (final device in devices) {
+          final services = await device.discoverServices();
+          if (services.any((s) => s.uuid == serviceUuid)) {
+            _connectedDevice = device;
+            setState(() {
+              statusMessage = "Connected!";
+              Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+              return;
+            });
+          }
+        }
+      }
+    } catch (e) {
+      print("Error fetching connected devices: $e");
+    }
+  }
 
   // function to start scanning for devices advertising the service
+  void _startScanning() {
+    FlutterBluePlus.startScan(
+      withServices: [serviceUuid],
+      timeout: const Duration(seconds: 999),
+    );
+    scanSubscription = FlutterBluePlus.scanResults.listen(
+      (results) {
+        setState(() {
+          scanResults = results;
+          for (var result in results) {
+            if (result.advertisementData.serviceUuids
+                .contains(serviceUuid.toString())) {
+              FlutterBluePlus.stopScan();
+              scanSubscription.cancel();
+              _connectToDevice(result.device);
+              return;
+            }
+          }
+          setState(() {
+            scanResults = results;
+            // If you wish, put a message here to indicate no devices found
+          });
+        });
+      },
+    );
+  }
 
   // function to attempt connecting to a device
+  Future<void> _connectToDevice(BluetoothDevice device) async {
+    try {
+      await device.connect();
+      final services = await device.discoverServices();
+      if (services.any((s) => s.uuid == serviceUuid)) {
+        setState(() {
+          _connectedDevice = device;
+          statusMessage = "Connected!";
+          Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+        });
+      } else {
+        setState(() {
+          statusMessage = "Failed to connect to device.";
+        });
+      }
+    } catch (e) {
+      setState(() {
+        statusMessage = "Error connecting to device: $e";
+      });
+    } finally {
+      scanSubscription.cancel();
+    }
+  }
 
   // function to disconnect from a device
-
-  // helper function to render changes in the UI
+  Future<void> _disconnectFromDevice() async {
+    if (_connectedDevice != null) {
+      await _connectedDevice!.disconnect();
+      setState(() {
+        _connectedDevice = null;
+        statusMessage = "Disconnected";
+      });
+    }
+  }
 
   @override
   void initState() {
     super.initState();
+    _checkConnectedDevices();
     _fetchConnectedDevices();
 
     _controller = AnimationController(
@@ -67,13 +145,16 @@ class _ConnectScreenState extends State<ConnectScreen>
   @override
   void dispose() {
     _controller.dispose();
-    scanSubscription?.cancel();
+    scanSubscription.cancel();
     super.dispose();
   }
 
   void _onBluetoothPressed() {
     if (_controller.isAnimating) {
       _controller.stop();
+      FlutterBluePlus.stopScan();
+      scanSubscription.cancel();
+      _disconnectFromDevice();
       setState(() {
         isScanning = false;
         statusMessage = "Not Connected";
@@ -84,6 +165,7 @@ class _ConnectScreenState extends State<ConnectScreen>
         isScanning = true;
         statusMessage = "Scanning...";
       });
+      _startScanning();
     }
 
     // other logic
@@ -127,24 +209,41 @@ class _ConnectScreenState extends State<ConnectScreen>
                   ),
                 ),
               ),
-              SizedBox(
-                  height: SizeConfig.blockSizeVertical! * 10,
-                  width: SizeConfig.blockSizeHorizontal! * 80,
-                  child: Card(
-                      color: Colors.white,
-                      child: Padding(
-                          padding: EdgeInsets.all(16),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("RepSync Device",
-                                  style: GoogleFonts.exo2(
-                                      fontSize: 20,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold)),
-                              Text(statusMessage)
-                            ],
-                          )))),
+              Column(
+                children: [
+                  SizedBox(
+                      height: SizeConfig.blockSizeVertical! * 11,
+                      width: SizeConfig.blockSizeHorizontal! * 80,
+                      child: Card(
+                          color: Colors.white,
+                          child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("RepSync Device",
+                                      style: GoogleFonts.exo2(
+                                          fontSize: 20,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold)),
+                                  Text(statusMessage)
+                                ],
+                              )))),
+                  (_connectedDevice != null)
+                      ? SizedBox(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 0, 0, 0),
+                                  foregroundColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              onPressed: () {},
+                              child: Text("Disconnect")),
+                        )
+                      : Container(),
+                ],
+              )
             ],
           ),
         ),
